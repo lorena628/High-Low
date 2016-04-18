06e80741de41d9393b9ccc79ba4411368de03903
lo v <lov@los-MacBook-Pro.local>
2016-04-18 11:19:54 -0500

    added arguments with a min/max

--- high_low.php
+++ high_low.php
@@ -1,20 +1,55 @@
 <?php
 
-$num = mt_rand(0, 100);
+// these are the new argumaents for min/max 
+$min = $argv[1];
+$max = $argv[2];
+ 
 
 
-do {
-echo "Pick a number\n";
+if($argv[1] < $argv[2]){
+    $min = $argv[1];
+    $max = $argv[2];
 
-$guess = trim(fgets(STDIN));
-
-if ($guess > $num) {
-    echo "Guess LOWER, ";
-
-} elseif ($guess < $num) {
-    echo "Guess HIGHER, ";
+}elseif($argv[1] > $argv[2]){
+    $max = $argv[1];
+    $min = $argv[2];
 }
-elseif ($guess == $num){
-    echo "You got it!";
+// if($arg < 3) {
+//     exit;
+// }
+
+// cheching if new arguments are numbers
+if(!is_numeric($min)){
+        echo "this is not a number\n";
+        exit;
 }
-}while ($guess != $num);
+if(!is_numeric($max)){
+    echo "this is not a number\n";
+    exit;
+} 
+// this is the do while for picking a number
+do{
+    $num = mt_rand($min, $max);
+
+    do {
+    echo "Pick a number\n";
+    //users guess
+    $guess = trim(fgets(STDIN));
+    //if guess is not a number
+    if (!is_numeric($guess)){
+        echo "That is not a number \n";
+    }elseif ($guess > $num) {
+        echo "Guess LOWER, ";
+
+    }elseif ($guess < $num) {
+        echo "Guess HIGHER, ";
+    }
+    elseif ($guess == $num){
+        echo "You got it! Play again?";
+        }
+    }while ($guess != $num);
+// this is the variable for users response to playing again
+$resp = trim(fgets(STDIN));
+}while ($resp == 'yes');
+echo "have a nice day\n";
+
